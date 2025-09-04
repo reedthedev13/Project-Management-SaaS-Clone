a0a0421f7e2a0fbd5a697bfd3dad12ca8ad20c22
Reed Lathrop <reedlathrop@gmail.com>
2025-09-04 11:28:40 -0500

    api client

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -1,6 +1,7 @@
 import { User } from "../contexts/UserContext";
 
 const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5001/api";
+console.log("BASE_URL:", BASE_URL);
 
 export async function apiRequest<T>(
   endpoint: string,
