9a4dae8b312b61ee4a40915770f933961605f856
Reed Lathrop <reedlathrop@gmail.com>
2025-09-04 11:46:22 -0500

    env and apiclient change

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -1,6 +1,6 @@
 import { User } from "../contexts/UserContext";
 
-const BASE_URL = import.meta?.env?.VITE_API_URL ?? "http://localhost:5001/api";
+const BASE_URL = import.meta.env.VITE_API_URL ?? "http://localhost:5001";
 console.log("BASE_URL:", BASE_URL);
 
 export async function apiRequest<T>(
